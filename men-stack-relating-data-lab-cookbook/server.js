48893e038841842fc77f6aa2149452806936280b
sedratiaziz <87585184+sedratiaziz@users.noreply.github.com>
2025-01-14 22:46:57 +0300

    initialized setup

--- men-stack-relating-data-lab-cookbook/server.js
+++ men-stack-relating-data-lab-cookbook/server.js
@@ -7,25 +7,15 @@ const methodOverride = require("method-override");
 const morgan = require("morgan");
 const session = require("express-session");
 
-const isSignedIn = require("./middleware/is-signed-in.js");
-
-const passUserToView = require("./middleware/pass-user-to-view.js");
-
-const applicationsController = require("./controllers/applications.js");
-
-const authController = require("./controllers/auth.js");
-
 const port = process.env.PORT ? process.env.PORT : "3000";
 
 mongoose.connect(process.env.MONGODB_URI);
-
 mongoose.connection.on("connected", () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride("_method"));
-// app.use(morgan('dev'));
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -34,26 +24,44 @@ app.use(
   })
 );
 
-app.use(passUserToView); // use new passUserToView middleware here
+// ****************************CODE***********************************
 
-// middleware/is-signed-in.js
 
-app.get("/", (req, res) => {
-  // Check if the user is signed in
-  if (req.session.user) {
-    // Redirect signed-in users to their applications index
-    res.redirect(`/users/${req.session.user.username}/applications`);
-  } else {
-    // Show the homepage for users who are not signed in
-    res.render("index.ejs");
-  }
-});
 
-app.use("/auth", authController);
 
-app.use(isSignedIn);
 
-app.use("/users/:username/applications", applicationsController);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ****************************CODE***********************************
+
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
